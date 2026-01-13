f83f039b61c6736ddd9de3e0ea1302a84e6943a8
Onofre Souza <souzaonofre@gmail.com>
2026-01-13 16:38:18 +0000

    Remove auth of dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
