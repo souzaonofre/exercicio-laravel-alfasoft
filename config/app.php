c77a46df5977219903ccb606a9df952556776fec
Onofre Souza <souzaonofre@gmail.com>
2026-01-13 21:40:11 +0000

    Add app config value to "Admin" username.

--- config/app.php
+++ config/app.php
@@ -185,4 +185,8 @@ return [
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
+
+    // config admin username
+    'admin_username' => env('APP_ADMIN_USERNAME', 'admin'),
+
 ];
